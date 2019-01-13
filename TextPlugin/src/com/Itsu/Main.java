3c025b9ac66d235b6bb5e3aea67075f583ee2bec
Itsu <itsu020402@gmail.com>
2019-01-13 11:45:54 +0900

    Add files via upload

---
+++ TextPlugin/src/com/Itsu/Main.java
@@ -0,0 +1,12 @@
+package com.Itsu;
+
+import cn.nukkit.event.Listener;
+import cn.nukkit.plugin.PluginBase;
+
+public class Main extends PluginBase implements Listener{
+	
+	public void onEnable(){
+		getLogger().info("Enabled.");
+	}
+
+}
